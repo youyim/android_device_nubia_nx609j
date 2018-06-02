3a522d8a18be1303016a4b035524771b495c6112
youyim123 <youyim@hotmail.com>
2018-06-02 20:41:53 +0800

    new sign boot method

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,7 +90,7 @@ TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_NO_USB_STORAGE := true
 TW_NEW_ION_HEAP := true
 BOARD_FIX_NUBIA_OTA := true
-BOARD_CUSTOM_BOOTIMG_MK := device/nubia/nx609j/mkbootimg.mk
+TW_IGNORE_MISC_WIPE_DATA := true
 
 #remove screen pointer
 TW_INPUT_BLACKLIST := "hbtp_vm"
